b265fba09cafbb7b19304e4099a7de28d98cc0bd
Rodrigo Mufatto <rodrigom3317@gmail.com>
2025-04-16 20:33:51 -0400

    feat: create util to load Google Maps API

---
+++ src/utils/load-google.ts
@@ -0,0 +1,18 @@
+
+import { Loader } from "@googlemaps/js-api-loader";
+
+let isLoaded = false;
+
+export async function loadGoogleMapsApi() {
+  if (isLoaded) return;
+
+  const loader = new Loader({
+    apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+    libraries: ["places"],
+    language: "pt-BR",
+    region: "BR",
+  });
+
+  await loader.load();
+  isLoaded = true;
+}
